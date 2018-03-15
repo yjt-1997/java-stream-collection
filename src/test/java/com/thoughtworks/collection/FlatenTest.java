4b9d252021e16e2da188f0c4dac44623dac5001d
Hong Ma <hma@thoughtworks.com>
2018-03-15 18:54:57 +0800

    add comment

--- src/test/java/com/thoughtworks/collection/FlatenTest.java
+++ src/test/java/com/thoughtworks/collection/FlatenTest.java
@@ -4,15 +4,17 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FlatenTest {
 
     @Test
-    public void can_transform_two_dimensional_array_to_one(){
-        Integer[][] array = new Integer[][]{{1}, {2}, {3,4}};
+    public void can_transform_two_dimensional_array_to_one() {
+        //把二维数组变成一维数组
+        Integer[][] array = new Integer[][]{{1}, {2}, {3, 4}};
 
-        Integer[] result = new Integer[]{1,2,3,4};
+        Integer[] result = new Integer[]{1, 2, 3, 4};
         List<Integer> resultList = Arrays.asList(result);
 
         Flaten flaten = new Flaten(array);
@@ -21,10 +23,11 @@ public class FlatenTest {
     }
 
     @Test
-    public void can_transform_two_dimensional_array_to_one_and_delete_repeated(){
-        Integer[][] array = {{1,2,3}, {5,2,1,4}, {2,1}};
+    public void can_transform_two_dimensional_array_to_one_and_delete_repeated() {
+        // 把二维数组变成一维数组,消除重复,按照第一次出现的顺序排列最后的输出结果
+        Integer[][] array = {{1, 2, 3}, {5, 2, 1, 4}, {2, 1}};
 
-        Integer[] result = {1,2,3,5,4};
+        Integer[] result = {1, 2, 3, 5, 4};
         List<Integer> resultList = Arrays.asList(result);
 
         Flaten flaten = new Flaten(array);
--- src/test/java/com/thoughtworks/collection/InverseReduceTest.java
+++ src/test/java/com/thoughtworks/collection/InverseReduceTest.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.collection;
 
 import org.junit.Test;
+
 import java.util.*;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -9,14 +11,14 @@ import static org.mockito.Mockito.when;
 public class InverseReduceTest {
 
     @Test
-    public void should_be_divided_to_smaller_number(){
-
+    public void should_be_divided_to_smaller_number() {
+     //根据给定数字无限分割至等于0(提示：9-2=7,7-2=5 ...)
         Random random = mock(Random.class);
         when(random.nextInt(3)).thenReturn(2);
 
-        InverseReduce inverseReduce = new InverseReduce(random);
+            InverseReduce inverseReduce = new InverseReduce(random);
 
-        Integer[] result = new Integer[]{7,5,3,1};
+        Integer[] result = new Integer[]{7, 5, 3, 1};
         List<Integer> resultList = Arrays.asList(result);
 
         assertThat(inverseReduce.divideToSmaller(9)).isEqualTo(resultList);
