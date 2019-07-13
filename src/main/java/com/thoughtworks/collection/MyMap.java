d8bc0f895e41b37e1e5d85b81be41b3da0e7bece
yjt-1997 <jiangtap1997qqq@qq.com>
2019-07-13 10:34:23 +0800

    pass all functions in map and commit

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -4,6 +4,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,11 +18,11 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return array.stream().map(i -> i * 3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream().map(i -> letters[i - 1]).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
@@ -29,10 +30,10 @@ public class MyMap {
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return array.stream().sorted((a, b) -> b - a).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream().sorted((a, b) -> a - b).collect(Collectors.toList());
     }
 }
